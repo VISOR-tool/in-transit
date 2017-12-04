7cb85f0d8325e0e2bca471973668ee8c6792d56c
Astro <astro@spaceboyz.net>
2017-12-05 00:07:17 +0100

    pureify Links, Process components

--- src/components/timeline/links.js
+++ src/components/timeline/links.js
@@ -1,48 +1,42 @@
 import { h, Component } from 'preact';
 
-export default class Links extends Component {
-  // constructor () {
-  //   super();
-  // }
+const Links = ({ processes }) => {
+  if(processes.length < 2) return false;
 
-  render () {
-    const { processes } = this.props;
-
-    if(processes.length < 2) return false;
-
-    let lines = [];
-    processes.map( (process,index,all) => {
-        if(process.attributes.process.visible == true){
-          all.map(pAll => {
-            if( pAll.attributes.process.visible == true &&
-                pAll.attributes.process.connection.from.indexOf(process.attributes.process.id) > -1)
-            {
-              lines.push({
-                    x1: process.attributes.processPosition.x + process.attributes.processPosition.width,
-                    y1: process.attributes.processPosition.y + process.attributes.processPosition.height,
-                    x2: pAll.attributes.processPosition.x ,
-                    y2: pAll.attributes.processPosition.y
-                  });
-            }
+  let lines = [];
+  processes.map( (process,index,all) => {
+    if(process.attributes.process.visible == true){
+      all.map(pAll => {
+        if( pAll.attributes.process.visible == true &&
+            pAll.attributes.process.connection.from.indexOf(process.attributes.process.id) > -1)
+        {
+          lines.push({
+            x1: process.attributes.processPosition.x + process.attributes.processPosition.width,
+            y1: process.attributes.processPosition.y + process.attributes.processPosition.height,
+            x2: pAll.attributes.processPosition.x ,
+            y2: pAll.attributes.processPosition.y
           });
         }
-    });
+      });
+    }
+  });
 
-    var attrs = {
-      stroke: '#D41A20',
-      'stroke-width': 1,
-    };
+  var attrs = {
+    stroke: '#D41A20',
+    'stroke-width': 1,
+  };
 
-    let svgLines = lines.map( line => (
-         <line x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2}  {...attrs} />
-         ));
+  let svgLines = lines.map( line => (
+      <line x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2}  {...attrs} />
+  ));
 
-        //links.map( link => ( <line x1="0" y1="0" x2="200" y2="200"  {...attrs} /> ) )
+  //links.map( link => ( <line x1="0" y1="0" x2="200" y2="200"  {...attrs} /> ) )
 
-    return (
-      <g>
+  return (
+    <g>
       {svgLines}
-      </g>
-    ) ;
-  }
+    </g>
+  );
 }
+
+export default Links;
--- src/components/timeline/process.js
+++ src/components/timeline/process.js
@@ -1,73 +1,68 @@
 import { h, Component } from 'preact';
 
-export default class Process extends Component {
-  // constructor () {
-  //   super();
-  // }
+const Process = ({ process, processPosition, stakeholder }) => {
+  if(process.visible == true)
+  {
+    let procAttrs = {
+      width: 80,
+      stroke: '#3784F8',
+      'stroke-width': 1,
+      fill: '#61D2E8'
+    };
 
-  render () {
-    const { process, processPosition, stakeholder } = this.props;
-    if(process.visible == true)
-    {
-      let procAttrs = {
+    if(process.searchHit){
+      procAttrs = {
         width: 80,
         stroke: '#3784F8',
         'stroke-width': 1,
-        fill: '#61D2E8'
+        fill: '#E88261'
       };
-
-      if(process.searchHit){
-        procAttrs = {
-          width: 80,
-          stroke: '#3784F8',
-          'stroke-width': 1,
-          fill: '#E88261'
-        };
-      }
-
-
-      const processAttrs = {
-        "font-family": "Verdana",
-        "font-size"  : 10,
-        "font-weight" : "bold"
-      }
-      const iniAttrs = {
-        "font-family": "Verdana",
-        "font-size"  : 10,
-      }
-      const spacer = 5;
-      const sh = stakeholder.find(sh => sh.id == process.initiator);
-      const processInitiator = sh.name;
-      return (
-        <g>
-          <rect id={process.id}
-                x={processPosition.x}
-                y={processPosition.y}
-                height={processPosition.height}
-                {...procAttrs}
-          />
-          <text x={processPosition.x + spacer}
-                y={processPosition.y + 9 }
-                {...processAttrs}
-                >{process.name}
-          </text>
-          <text x={processPosition.x + spacer}
-                y={processPosition.y + 20}
-                {...iniAttrs}
-                >Ini:{processInitiator}
-          </text>
-          <text x={processPosition.x + spacer}
-                y={processPosition.y + 30}
-                {...iniAttrs}
-                >[{process.participation}]
-          </text>
-          <text x={processPosition.x + spacer}
-                y={processPosition.y + 40}
-                {...iniAttrs}
-                >[{process.start}]
-          </text>
-        </g>
-      );
     }
+
+
+    const processAttrs = {
+      "font-family": "Verdana",
+      "font-size"  : 10,
+      "font-weight" : "bold"
+    }
+    const iniAttrs = {
+      "font-family": "Verdana",
+      "font-size"  : 10,
+    }
+    const spacer = 5;
+    const sh = stakeholder.find(sh => sh.id == process.initiator);
+    const processInitiator = sh.name;
+    return (
+      <g>
+        <rect id={process.id}
+              x={processPosition.x}
+              y={processPosition.y}
+              height={processPosition.height}
+              {...procAttrs}
+        />
+        <text x={processPosition.x + spacer}
+              y={processPosition.y + 9 }
+              {...processAttrs}
+              >{process.name}
+        </text>
+        <text x={processPosition.x + spacer}
+              y={processPosition.y + 20}
+              {...iniAttrs}
+              >Ini:{processInitiator}
+        </text>
+        <text x={processPosition.x + spacer}
+              y={processPosition.y + 30}
+              {...iniAttrs}
+              >[{process.participation}]
+        </text>
+        <text x={processPosition.x + spacer}
+              y={processPosition.y + 40}
+              {...iniAttrs}
+              >[{process.start}]
+        </text>
+      </g>
+    );
   }
 }
+
+export default Process;
