8553f14bb3e1b0e9c5479a88169efb3dbc646f9b
rob.tranquillo <rob.tranquillo@gmx.de>
2018-01-31 18:56:17 +0100

    better (not best) collision detection

--- src/components/timeline/swimlane.js
+++ src/components/timeline/swimlane.js
@@ -9,29 +9,26 @@ class Swimlane extends Component {
     super();
   }
 
-  processMargin = 30;
   processWidth = 80;
   spacer = 5;
   stacking = { base:false, count: 0 };
-  stackingCollisonTable = [];
 
-  processPositionX = function(process, tlX, tlY){
+  processPositionX = function(process, tlX, tlY, width){
     //from connections mimize staking.base
     if( process.connection.from.length > 1 ) {
       this.stacking.base =
         this.stacking.base - process.connection.from.length - 1;
     }
 
-    //reset values on last from connection 
+    //reset values on last from connection
     if(this.stacking.base < 1) {
         this.stacking.base = 1;
         this.stacking.count = 0;
     }
-    //increase the staking.count for every child 
+    //increase the staking.count for every child
     if(this.stacking.base > 1) this.stacking.count++;
     if(this.stacking.base == 1) this.stacking.count=0;
 
-    let height = (this.props.height - this.spacer) / this.stacking.base;
     if( process.connection.to.length > 1 ) {
       this.stacking.base += (process.connection.to.length - 1);
     }
@@ -39,48 +36,55 @@ class Swimlane extends Component {
     let startPx = this.props.width/(this.props.zoomEnd.valueOf()-this.props.zoomStart.valueOf())
                   *(Date.parse(process.start)-this.props.zoomStart.valueOf())
                   +tlX;
-    
+
     let stacking = 0
     if(this.stacking.count > 1) stacking = this.stacking.count - 1;
 
-    let a = {
+    return {
+      id: process.id,
+      visible: startPx > width ? false : true,
       x: startPx,
-      y: -1,
-      height: height,
       width: this.processWidth,
+      stackingOrder: 1,
       };
-      //console.log(a);
-      return a;
+    }
+
+    getSimultaneousObjects (thisElement, allPositions){
+      const thisStart = thisElement.x;
+      const thisEnd = thisElement.x + thisElement.width;
+      let simCounts = {all: 0, iterator: 0};
+      allPositions.forEach( proc => {
+        if( thisElement != proc &&            
+            ! (proc.x < thisStart && proc.x + proc.width < thisStart
+            || proc.x > thisEnd && proc.x + proc.width > thisEnd))
+            {
+              simCounts.all++;
+              if(proc.y != undefined) simCounts.iterator++
+            }
+          });
+        return simCounts;
+    }
+
+    processPositionY (thisElement, tlY, processPositions) {
+      //find stackings and place the elements vertical
+      if( ! thisElement.visible || thisElement.y != undefined ) return;
+      let stackedObjectsCount = this.getSimultaneousObjects (thisElement, processPositions);
+      thisElement.height = (this.props.height - this.spacer) / (stackedObjectsCount.all+1);
+      thisElement.y = (thisElement.height * stackedObjectsCount.iterator) + tlY;
+      if(thisElement.y == NaN)
+        console.log(thisElement.y, thisElement.height, stackedObjectsCount);
+      return thisElement;
   }
 
-
-  processPositionY ( thisElement, processPositions) {    
-    //collisions detection
-    const thisStart = thisElement.x;
-    const thisEnd = thisElement.x + thisElement.width;
-    let stackedElements = 0;
-    let highestY = 0;
-    processPositions.forEach( proc => {
-      if( thisStart >= proc.x && thisStart <= proc.x + proc.width 
-          || thisEnd >= proc.x && thisEnd <= proc.x + proc.width )
-          stackedElements++;
-      if(highestY < proc.y) highestY = proc.y;
-    });
-    thisElement.height = thisElement.height / stackedElements;
-    if(stackedElements) 
-    thisElement.y = highestY + thisElement.height;
-  }
-  
   render () {
     const { id, title, x, y, width, height, processes, stakeholder } = this.props;
     this.stacking.base = 1;
     this.stacking.count = 0;
     let processPositions = [];
-    processPositions = processes.map(process => this.processPositionX( process, x, y)); 
-    this.stacking.count = 0;
-    processPositions.map( pos => this.processPositionY( pos, processPositions )); 
-    
-    
+    processPositions = processes.map(process => this.processPositionX( process, x, width));
+    this.stacking.count = 1;
+    processPositions.map( pos => pos = this.processPositionY( pos, y, processPositions ));
+
     var timelineAttrs = {
       stroke: '#16CEEA',
       'stroke-width': 1,
@@ -105,7 +109,6 @@ class Swimlane extends Component {
     let processObjs = processes.map( (process, index) =>
             <Process
               process = {process}
-              //processPosition = {this.processPosition(process, x, y)}
               processPosition = {processPositions[index]}
               stakeholder = {stakeholder}
             />
--- src/routes/timeline/index.js
+++ src/routes/timeline/index.js
@@ -46,7 +46,7 @@ class TimelineRoute extends Component {
           <Filtering />
         </div>
         <div class={style.workspace}>
-          <SimplGraph  width={640} height={100} />
+          <SimplGraph  width={640} height={100} />        
           <Timeline
             width={window.outerWidth - 5}
             height={window.outerHeight - 300}
