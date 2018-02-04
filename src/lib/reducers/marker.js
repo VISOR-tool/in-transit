884043a28e02ad6c94135bec1b81ffd11192eb5e
Astro <astro@spaceboyz.net>
2018-02-04 23:25:57 +0100

    reducers/marker: fix data model

--- src/lib/reducers/marker.js
+++ src/lib/reducers/marker.js
@@ -1,24 +1,24 @@
-const INITIAL_STATE = {
-  marker: [ {id:'foo', x:331, process:"proc/11"} ],
-};
+const INITIAL_STATE = [
+  {id:'foo', x:331, process:"proc/11"},
+];
 
 const ADD_MARKER = 'visor/data/ADD_MARKER';
 
 export function markerReducer(allMarker = INITIAL_STATE, action) {
   switch(action['type']) {
   case ADD_MARKER:
-    return {
-      ...allMarker.push( action.marker ),
-    };
-  case GET_ALL_MARKER:
+    return allMarker.concat(action.marker);
+  default:
     return allMarker;
   }
 }
 
 const markerActions = {
-  addMarker: (id,position) => ({
+  addMarker: (id, position) => ({
     type: ADD_MARKER,
-    id,
-    position,
+    marker: {
+      id,
+      position,
+    },
   }),
 }
