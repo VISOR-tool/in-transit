9351f53e50bfddca28a9bf1e0ab88ce8bafa5231
rob.tranquillo <rob.tranquillo@gmx.de>
2018-02-19 09:37:43 +0100

    rename header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render () {
     return (
       <header class={style.header}>
-        <h3>visor - Städtebauliche Pläne durchforsten</h3>
+        <h3>visor</h3>
         <nav>
           <Link activeClassName={style.active} href='/'>Home</Link>
           <Link activeClassName={style.active} href='/timeline'>Timeline</Link>
