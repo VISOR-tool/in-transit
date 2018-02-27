2bdf6de31620957a2f6d42ea91b851fd9a018c94
rob.tranquillo <rob.tranquillo@gmx.de>
2018-02-27 22:13:15 +0100

    war on colors :D

--- src/components/relationsGraph/legend.js
+++ src/components/relationsGraph/legend.js
@@ -1,16 +1,18 @@
 import { h, Component } from 'preact';
 
+const text_color = "#00000098";
+
 function Item({x, y, stroke, fill, label}) {
   return <g>
     <circle cx={x} cy={y + 8} r={4} stroke={stroke} stroke-width={4 / 3} fill={fill}/>
-    <text x={x + 8} y={y + 12}>{label}</text>
+    <text fill={text_color} x={x + 8} y={y + 12}>{label}</text>
   </g>;
 }
 
 export default function Legend({x, y, width}) {
   return (
     <g>
-      <text x={x + 20} y={y + 12} font-weight="bold">Legende</text>
+      <text x={x + 20} y={y + 12} fill={text_color} font-weight="bold">Legende</text>
       <Item x={x + 100} y={y} stroke='#989899' fill='#989899' label='Beteiligung geschlossen'/>
       <Item x={x + 250} y={y} stroke='#FFB124' fill='#FFB124' label='Beteiligung offen'/>
       <Item x={x + 400} y={y} stroke='#ff6927' fill='white' label='Selektierter Prozess'/>
